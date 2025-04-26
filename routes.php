5fcaffa6c2276be96d33238b112e301ca4088267
kdpenha <kayke.rodrigues@hubcontrol.com.br>
2025-04-26 13:37:57 -0300

    chore: refatorando routes para novas rotas

--- routes.php
+++ routes.php
@@ -2,16 +2,29 @@
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-switch ($uri) {
-    case '/home':
-    case '/':
-        require 'controllers/HomeController.php';
-        $controller = new HomeController();
-        $controller->index();
-        break;
-    
-    default:
-        http_response_code(404);
-        echo "Página não encontrada!";
-        break;
+$routes = [
+    '/' => ['controller' => 'HomeController', 'method' => 'index'],
+    '/home' => ['controller' => 'HomeController', 'method' => 'index'],
+
+    '/properties' => ['controller' => 'PropertyController', 'method' => 'index'],
+    '/properties/create' => ['controller' => 'PropertyController', 'method' => 'create'],
+    '/properties/save' => ['controller' => 'PropertyController', 'method' => 'save'],
+    '/properties/edit' => ['controller' => 'PropertyController', 'method' => 'edit'],
+    '/properties/update' => ['controller' => 'PropertyController', 'method' => 'update'],
+    '/properties/delete' => ['controller' => 'PropertyController', 'method' => 'delete'],
+];
+
+// Verifica se a rota existe
+if (isset($routes[$uri])) {
+    $controllerName = $routes[$uri]['controller'];
+    $method = $routes[$uri]['method'];
+
+    require_once __DIR__ . "/controllers/{$controllerName}.php";
+
+    $controller = new $controllerName();
+
+    call_user_func([$controller, $method]);
+} else {
+    http_response_code(404);
+    echo "Página não encontrada!";
 }
