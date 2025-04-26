e5568714dab99ae7a5b344ac07c01b03fc38b372
kdpenha <kayke.rodrigues@hubcontrol.com.br>
2025-04-26 12:07:16 -0300

    feat: definindo variaveis de bairros e tipos de imoveis

---
+++ config/constants.php
@@ -0,0 +1,19 @@
+<?php
+
+// Tipos de imóveis
+$propertyTypes = [
+    'Apartamento',
+    'Casa',
+    'Sobrado',
+    'Kitnet',
+    'Chácara'
+];
+
+// Bairros
+$neighborhoods = [
+    'Centro',
+    'Zona Norte',
+    'Zona Sul',
+    'Zona Leste',
+    'Zona Oeste'
+];
